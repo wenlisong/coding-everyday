da60bf8437fc824eb3cb8b6ecd5422abca18dc1e
songwenli <327361201@qq.com>
2019-06-17 11:17:22 +0800

    MedianofTwoSortedArrays

---
+++ ArraySearch/MedianofTwoSortedArrays.py
@@ -0,0 +1,68 @@
+# There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+# Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+# You may assume nums1 and nums2 cannot be both empty.
+
+# Example 1:
+
+# nums1 = [1, 3]
+# nums2 = [2]
+
+# The median is 2.0
+# Example 2:
+
+# nums1 = [1, 2]
+# nums2 = [3, 4]
+
+# The median is (2 + 3)/2 = 2.5
+
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        m, n = len(nums1), len(nums2)
+        if m > n:
+            nums1, nums2 = nums2, nums1
+            m, n = n, m
+        if n == 0:
+            raise ValueError
+        
+        imin, imax, half_length = 0, m, (m + n + 1) // 2
+        # print(nums1, nums2)
+        while imin <= imax:
+            i = (imin + imax) // 2
+            j = half_length - i
+            # print(i, j)
+            if i < m and nums2[j - 1] > nums1[i]:
+                imin = i + 1
+            elif i > 0 and nums1[i - 1] > nums2[j]:
+                imax = i - 1
+            else:
+                if i == 0:
+                    max_of_left = nums2[j - 1]
+                elif j == 0:
+                    max_of_left = nums1[i - 1]
+                else:
+                    max_of_left = max(nums1[i - 1], nums2[j - 1])
+                
+                if (m + n) % 2 == 1:
+                    return max_of_left
+                
+                if i == m:
+                    min_of_right = nums2[j]
+                elif j == n:
+                    min_of_right = nums1[i]
+                else:
+                    min_of_right = min(nums1[i], nums2[j])
+                # print(max_of_left, min_of_right)
+                return (max_of_left + min_of_right) / 2
+
+if __name__ == "__main__":
+    solu = Solution()
+    res = solu.findMedianSortedArrays([1, 2], [-1, 3])
+    print('result: {}'.format(res))
+                
