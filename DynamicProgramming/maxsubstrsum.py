e8dd21494c0e4478de8a4de1112a57f681880b10
songwenli <sfswl@qq.com>
2019-08-16 16:00:08 +0800

    最大连续子序列的和

---
+++ DynamicProgramming/maxsubstrsum.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+
+# 在古老的一维模式识别中,常常需要计算连续子向量的最大和,当向量全为正数的时候,问题很好解决。
+# 但是,如果向量中包含负数,是否应该包含某个负数,并期望旁边的正数会弥补它呢？
+# 例如:{6,-3,-2,7,-15,1,2,2},连续子向量的最大和为8(从第0个开始,到第3个为止)。
+# 给一个数组，返回它的最大连续子序列的和，你会不会被他忽悠住？(子向量的长度至少是1)
+class Solution:
+    def FindGreatestSumOfSubArray(self, array):
+        dp = array
+        for i in range(1, len(array)):
+            dp[i] = max(array[i], dp[i-1]+array[i])
+        return max(dp)
+
+
+if __name__ == "__main__":
+    solu = Solution()
+    res = solu.FindGreatestSumOfSubArray([6,-3,-2,7,-15,1,2,2])
+    print(res)
