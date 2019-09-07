519f27f6b60611f11889de83259d05dce68791b1
songwenli <sfswl@qq.com>
2019-09-07 10:19:13 +0800

    113. Path Sum II

---
+++ Tree/PathSumII.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+
+// 113. Path Sum II
+
+// Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+
+// Note: A leaf is a node with no children.
+
+// Example:
+// Given the below binary tree and sum = 22,
+
+//       5
+//      / \
+//     4   8
+//    /   / \
+//   11  13  4
+//  /  \    / \
+// 7    2  5   1
+// Return:
+
+// [
+//    [5,4,11,2],
+//    [5,8,4,5]
+// ]
+
+// Definition for a binary tree node.
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<Integer> path = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(root, sum, path, ans);
+        return ans;
+    }
+
+    // Note: Because we use reference, every item should be deleted after using.
+    private void helper(TreeNode root, int sum, List<Integer> path, List<List<Integer>> ans) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            if (root.val == sum) {
+                path.add(root.val);
+                ans.add(new ArrayList<>(path));
+                path.remove(path.size() - 1);
+            }
+            return;
+        }
+        path.add(root.val);
+        helper(root.left, sum - root.val, path, ans);
+        helper(root.right, sum - root.val, path, ans);
+        path.remove(path.size() - 1);
+    }
+}
