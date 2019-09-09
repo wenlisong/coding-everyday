5edacbc0a735eb528893ef1d27045f81bae9ec34
songwenli <sfswl@qq.com>
2019-09-09 11:44:36 +0800

    139. Word Break(Recusion)

--- DynamicProgramming/WordBreak.java
+++ DynamicProgramming/WordBreak.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 // 139. Word Break
@@ -27,15 +29,16 @@ import java.util.Set;
 // Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
 // Output: false
 class WordBreak {
+    // DP 5 ms 36.2 MB
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> words = new HashSet<>(wordDict);
         final int n = s.length();
-        boolean[] dp = new boolean[n+1];
+        boolean[] dp = new boolean[n + 1];
         dp[0] = true;
-        for(int l=1;l<=n;l++){
-            for(int i=0;i<l;i++){
-                if(dp[i]){
-                    if(words.contains(s.substring(i, l))){
+        for (int l = 1; l <= n; l++) {
+            for (int i = 0; i < l; i++) {
+                if (dp[i]) {
+                    if (words.contains(s.substring(i, l))) {
                         dp[l] = true;
                         break;
                     }
@@ -43,6 +46,32 @@ class WordBreak {
             }
         }
         return dp[n];
+    }
+
+    // Recusion 5 ms	36.4 MB
+    public boolean wordBreak2(String s, List<String> wordDict) {
+        Set<String> words = new HashSet<>(wordDict);
+        Map<String, Boolean> mem = new HashMap<>();
+        return healper(s, words, mem);
+    }
+
+    private boolean healper(String s, Set<String> words, Map<String, Boolean> mem) {
+        if (mem.containsKey(s)) {
+            return mem.get(s);
+        }
+        if (words.contains(s)) {
+            mem.put(s, true);
+            return true;
+        }
+        for (int i = 1; i < s.length(); i++) {
+            String left = s.substring(0, i), right = s.substring(i, s.length());
+            if (words.contains(right) && healper(left, words, mem)) {
+                mem.put(s, true);
+                return true;
+            }
+        }
+        mem.put(s, false);
+        return false;
     }
 
     public static void main(String[] args) {
