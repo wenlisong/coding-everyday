8e76212fbe0ee18d904a752082b80794265b3021
songwenli <sfswl@qq.com>
2019-09-14 20:34:42 +0800

    PathSum

--- Tree/PathSum.java
+++ Tree/PathSum.java
@@ -17,7 +17,7 @@
 // 7    2      1
 // return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
 
-class Solution {
+class PathSum {
     // Definition for a binary tree node.
     public class TreeNode {
         int val;
