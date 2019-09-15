886e3ae4c4661cd4275a90a1eeb0683a2b353e21
songwenli <sfswl@qq.com>
2019-09-15 11:17:36 +0800

    Merge Array with ArraySearch

--- Array/MedianofTwoSortedArrays.py
+++ Array/MedianofTwoSortedArrays.py
@@ -1,10 +1,21 @@
 # There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
 # Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
 # You may assume nums1 and nums2 cannot be both empty.
+
 # Example 1:
+
 # nums1 = [1, 3]
 # nums2 = [2]
+
 # The median is 2.0
+# Example 2:
+
+# nums1 = [1, 2]
+# nums2 = [3, 4]
+
+# The median is (2 + 3)/2 = 2.5
 
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
@@ -14,42 +25,44 @@ class Solution:
         :rtype: float
         """
         m, n = len(nums1), len(nums2)
-        if m == 0:
-            if n % 2:
-                return nums2[n / 2]
+        if m > n:
+            nums1, nums2 = nums2, nums1
+            m, n = n, m
+        if n == 0:
+            raise ValueError
+        
+        imin, imax, half_length = 0, m, (m + n + 1) // 2
+        # print(nums1, nums2)
+        while imin <= imax:
+            i = (imin + imax) // 2
+            j = half_length - i
+            # print(i, j)
+            if i < m and nums2[j - 1] > nums1[i]:
+                imin = i + 1
+            elif i > 0 and nums1[i - 1] > nums2[j]:
+                imax = i - 1
             else:
-                return (nums2[n / 2 - 1] + nums2[n / 2]) / 2
-        elif n == 0:
-            if m % 2:
-                return nums2[m / 2]
-            else:
-                return (nums2[m / 2 - 1] + nums2[m / 2]) / 2
-            
-        i = j = 0
-        tmp1 = tmp2 = None
-        while i + j < (m + n) / 2:
-            tmp1 = nums1[i]
-            tmp2 = nums2[j]
-            if nums1[i] < nums2[j]:
-                i = (m - i) / 2
-            else:
-                j = (n - j) / 2
-        if (m + n) % 2:
-            if nums1[i] == tmp1:
-                return tmp2
-            else:
-                return tmp1
-        else:
-            if nums1[i] == tmp1:
-                return (nums2[j - 1] + nums2[j]) / 2
+                if i == 0:
+                    max_of_left = nums2[j - 1]
+                elif j == 0:
+                    max_of_left = nums1[i - 1]
+                else:
+                    max_of_left = max(nums1[i - 1], nums2[j - 1])
                 
-
-def main():
-    nums1 = [1, 3]
-    nums2 = [2]
-    solu = Solution()
-    res = solu.findMedianSortedArrays(nums1, nums2)
-    print(res)
+                if (m + n) % 2 == 1:
+                    return max_of_left
+                
+                if i == m:
+                    min_of_right = nums2[j]
+                elif j == n:
+                    min_of_right = nums1[i]
+                else:
+                    min_of_right = min(nums1[i], nums2[j])
+                # print(max_of_left, min_of_right)
+                return (max_of_left + min_of_right) / 2
 
 if __name__ == "__main__":
-    main()
+    solu = Solution()
+    res = solu.findMedianSortedArrays([1, 2], [-1, 3])
+    print('result: {}'.format(res))
+                
