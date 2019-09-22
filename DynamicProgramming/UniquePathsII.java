1fdbd312ca2e6390901fad7d642df720c0d58c5a
songwenli <sfswl@qq.com>
2019-09-22 11:05:48 +0800

    63. Unique Paths II

---
+++ DynamicProgramming/UniquePathsII.java
@@ -0,0 +1,49 @@
+// 63. Unique Paths II
+
+// A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+// The robot can only move either down or right at any point in time. 
+// The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+// Now consider if some obstacles are added to the grids. How many unique paths would there be?
+
+// An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+
+// Note: m and n will be at most 100.
+
+// Example 1:
+// Input:
+// [
+//   [0,0,0],
+//   [0,1,0],
+//   [0,0,0]
+// ]
+// Output: 2
+// Explanation:
+// There is one obstacle in the middle of the 3x3 grid above.
+// There are two ways to reach the bottom-right corner:
+// 1. Right -> Right -> Down -> Down
+// 2. Down -> Down -> Right -> Right
+
+class UniquePathsII {
+    // DP 0 ms 38.3 MB
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (i == 1 && j == 1) {
+                    dp[i][j] = 1 - obstacleGrid[i - 1][j - 1];
+                }
+                if (obstacleGrid[i - 1][j - 1] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
